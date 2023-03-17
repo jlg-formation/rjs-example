02921cbd9da6172c590b357df3cf704452dba96b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-17 14:21:14 +0100

    dev: footer on bottom

--- front/src/layout/VerticalLayout.tsx
+++ front/src/layout/VerticalLayout.tsx
@@ -1,13 +1,21 @@
+import { css } from '@emotion/react'
 import BodyLayout from './BodyLayout'
 import FooterLayout from './FooterLayout'
 import HeaderLayout from './HeaderLayout'
 
 const VerticalLayout = () => (
-  <>
+  <div css={style}>
     <HeaderLayout />
     <BodyLayout />
     <FooterLayout />
-  </>
+  </div>
 )
 
 export default VerticalLayout
+
+const style = css`
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`
