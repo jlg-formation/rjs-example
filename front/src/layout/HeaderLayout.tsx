908feb8c201c7407f1787f203dddc1fbf5f4c350
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-17 13:15:59 +0100

    dev: added header dom and css

--- front/src/layout/HeaderLayout.tsx
+++ front/src/layout/HeaderLayout.tsx
@@ -1,3 +1,13 @@
-const HeaderLayout = () => <p>Header Layout ok</p>
+import logo from '/logo.svg'
+import s from './HeaderLayout.module.scss'
+
+const HeaderLayout = () => (
+  <header className={s.header}>
+    <a href="#">
+      <img src={logo} alt="Logo" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
+)
 
 export default HeaderLayout
