f79a705dfa43b388bacca319b2be11a7a81ba6e8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-17 17:20:04 +0100

    dev: added react loop

--- front/src/stock/views/ListView.tsx
+++ front/src/stock/views/ListView.tsx
@@ -1,51 +1,44 @@
 import { css } from '@emotion/react'
 import { Link } from 'react-router-dom'
 
-const ListView = () => (
-  <main css={s}>
-    <h1>Liste des articles</h1>
-    <div className="content">
-      <nav>
-        <button>Rafraîchir</button>
-        <Link to="add" className="button">
-          Ajouter
-        </Link>
-        <button>Supprimer</button>
-      </nav>
-      <table>
-        <thead>
-          <tr>
-            <th className="name">Nom</th>
-            <th className="price">Prix</th>
-            <th className="qty">Quantité</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="name">Tournevis</td>
-            <td className="price">3.99 €</td>
-            <td className="qty">123</td>
-          </tr>
-          <tr>
-            <td className="name">Pelle</td>
-            <td className="price">8.00 €</td>
-            <td className="qty">50</td>
-          </tr>
-          <tr>
-            <td className="name">Marteau</td>
-            <td className="price">5.50 €</td>
-            <td className="qty">34</td>
-          </tr>
-          <tr>
-            <td className="name">Rateau</td>
-            <td className="price">12.00 €</td>
-            <td className="qty">4</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </main>
-)
+const ListView = () => {
+  const articles = [
+    { id: 'a1', name: 'Tournevis', price: 2.99, qty: 11 },
+    { id: 'a2', name: 'Marteau', price: 5, qty: 145 },
+  ]
+  return (
+    <main css={s}>
+      <h1>Liste des articles</h1>
+      <div className="content">
+        <nav>
+          <button>Rafraîchir</button>
+          <Link to="add" className="button">
+            Ajouter
+          </Link>
+          <button>Supprimer</button>
+        </nav>
+        <table>
+          <thead>
+            <tr>
+              <th className="name">Nom</th>
+              <th className="price">Prix</th>
+              <th className="qty">Quantité</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((a) => (
+              <tr key={a.id}>
+                <td className="name">{a.name}</td>
+                <td className="price">{a.price} €</td>
+                <td className="qty">{a.qty}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
+}
 
 export default ListView
 
