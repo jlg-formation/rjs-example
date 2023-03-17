805a96b33cb11bf7dfb404bb5692d6e66e399922
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-17 13:29:32 +0100

    dev: css local with @emotion/css

--- front/src/layout/FooterLayout.tsx
+++ front/src/layout/FooterLayout.tsx
@@ -1,3 +1,26 @@
-const FooterLayout = () => <p>Footer Layout ok</p>
+import { css } from '@emotion/css'
+
+const FooterLayout = () => (
+  <footer className={s}>
+    <a href="#">Mentions Légales</a>
+  </footer>
+)
 
 export default FooterLayout
+
+const s = css`
+  height: 3em;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > a {
+    text-decoration: inherit;
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
