70ae644aba56f360fd3f80075b34f62fb625db6b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-17 11:58:49 +0100

    refactor: rewrite body in class

--- front/src/layout/BodyLayout.tsx
+++ front/src/layout/BodyLayout.tsx
@@ -1,3 +1,9 @@
-const BodyLayout = () => <p>Body Layout ok</p>
+import { Component, ReactNode } from 'react'
+
+class BodyLayout extends Component {
+  override render(): ReactNode {
+    return <p>Body Layout ok</p>
+  }
+}
 
 export default BodyLayout
