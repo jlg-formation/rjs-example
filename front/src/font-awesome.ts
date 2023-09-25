116a229ee486502b92c98c5058a9d050352b69b6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-25 16:46:21 +0200

    dev: added fas

--- front/src/font-awesome.ts
+++ front/src/font-awesome.ts
@@ -1,10 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faPlus,
-  faAngleRight,
-  faTrashAlt,
-  faRotateRight,
-  faCircleNotch,
-} from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlus, faAngleRight, faTrashAlt, faRotateRight, faCircleNotch)
+library.add(fas)
