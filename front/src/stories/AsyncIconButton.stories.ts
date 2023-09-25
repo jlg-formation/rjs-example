04fe9c74b6e7173d06a0e8a682b1dffa1125c938
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-25 16:28:09 +0200

    dev: remove comment

--- front/src/stories/AsyncIconButton.stories.ts
+++ front/src/stories/AsyncIconButton.stories.ts
@@ -1,18 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
-
 import AsyncIconButton from '../widgets/AsyncIconButton'
-
 import '../index.scss'
 import { sleep } from '../misc'
 import '../font-awesome'
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Gestion Stock/AsyncIconButton',
   component: AsyncIconButton,
   tags: ['autodocs'],
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
 } satisfies Meta<typeof AsyncIconButton>
@@ -26,7 +22,6 @@ const waitFor = (delay: number) => async () => {
 export default meta
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: {
     title: 'Click Me',
