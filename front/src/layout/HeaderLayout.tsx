8799907c5ffdd6e9d160478909be53f8f3289b31
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-01 12:08:58 +0200

    dev: dark theme svg logo

--- front/src/layout/HeaderLayout.tsx
+++ front/src/layout/HeaderLayout.tsx
@@ -1,16 +1,23 @@
-import logo from '/logo.svg'
+import logoLight from '/logo.svg'
+import logoDark from '/logo-dark.svg'
 import s from './HeaderLayout.module.scss'
 import { Link } from 'react-router-dom'
 import { ThemeSwitch } from '../widgets/ThemeSwitch'
+import { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
-const HeaderLayout = () => (
-  <header className={s.header}>
-    <Link to="/">
-      <img src={logo} alt="Logo" />
-      <span>Gestion Stock</span>
-    </Link>
-    <ThemeSwitch />
-  </header>
-)
+const HeaderLayout = () => {
+  const { theme } = useContext(ThemeContext)
+  const logo = theme === 'dark' ? logoDark : logoLight
+  return (
+    <header className={s.header}>
+      <Link to="/">
+        <img src={logo} alt="Logo" />
+        <span>Gestion Stock</span>
+      </Link>
+      <ThemeSwitch />
+    </header>
+  )
+}
 
 export default HeaderLayout
