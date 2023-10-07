ab6adfe75e77ddd81247fc40852bf9ea45846d1f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-07 12:06:10 +0200

    dev: form error management

--- front/src/stock/views/AddView.tsx
+++ front/src/stock/views/AddView.tsx
@@ -1,37 +1,55 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  ChangeEvent,
-  Dispatch,
-  FormEvent,
-  SetStateAction,
-  useState,
-} from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Title } from '../../widgets/Title'
 import { NewArticle } from '../interfaces/Article'
 import { useArticleStore } from '../store/ArticleStore'
 
-const handleChange =
-  <T,>(setState: Dispatch<SetStateAction<T>>) =>
-  (event: ChangeEvent<HTMLInputElement>) => {
-    setState(event.target.value as T)
+const validate = (newArticle: NewArticle) => {
+  const result = { name: '', price: '', qty: '' }
+  if (newArticle.name === '') {
+    result.name = 'Champs requis'
   }
+  if (newArticle.name.length > 10) {
+    result.name = `Trop long (${newArticle.name.length}>10)`
+  }
+  if (newArticle.price < 0) {
+    result.price = 'Prix negatif'
+  }
+  if (newArticle.qty < 0) {
+    result.qty = 'Prix negatif'
+  }
+  return result
+}
 
 const AddView = () => {
-  const [name, setName] = useState('Truc')
-  const [price, setPrice] = useState(0)
-  const [qty, setQty] = useState(0)
+  const [form, setForm] = useState({
+    value: { name: 'Truc', price: 0, qty: 0 },
+    error: { name: '', price: '', qty: '' },
+  })
 
   const [isAdding, setIsAdding] = useState(false)
 
   const { add, refresh } = useArticleStore()
   const navigate = useNavigate()
 
+  const handleChange =
+    (name: string) => (event: ChangeEvent<HTMLInputElement>) => {
+      const newForm = { ...form }
+      newForm.value = { ...form.value, [name]: event.target.value }
+      newForm.error = validate(newForm.value)
+      setForm(newForm)
+    }
+
   const handleSubmit = async (event: FormEvent<HTMLElement>) => {
     try {
       event.preventDefault()
       setIsAdding(true)
-      const newArticle: NewArticle = { name, price, qty }
+      const newArticle: NewArticle = {
+        name: form.value.name,
+        price: +form.value.price,
+        qty: +form.value.qty,
+      }
       await add(newArticle)
       await refresh()
       navigate('..')
@@ -48,19 +66,30 @@ const AddView = () => {
       <form onSubmit={handleSubmit}>
         <label>
           <span>Nom</span>
-          <input type="text" value={name} onChange={handleChange(setName)} />
+          <input
+            type="text"
+            value={form.value.name}
+            onChange={handleChange('name')}
+          />
+          <span className="error">{form.error.name}</span>
         </label>
         <label>
           <span>Prix</span>
           <input
             type="number"
-            value={price}
-            onChange={handleChange(setPrice)}
+            value={form.value.price}
+            onChange={handleChange('price')}
           />
+          <span className="error">{form.error.price}</span>
         </label>
         <label>
           <span>Quantité</span>
-          <input type="number" value={qty} onChange={handleChange(setQty)} />
+          <input
+            type="number"
+            value={form.value.qty}
+            onChange={handleChange('qty')}
+          />
+          <span className="error">{form.error.qty}</span>
         </label>
         <button className="primary" disabled={isAdding}>
           <FontAwesomeIcon
