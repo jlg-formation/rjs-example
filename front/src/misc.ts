340392589a5e86aca568dc3aaed774426557b760
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-18 12:05:36 +0200

    dev: added debounce function

--- front/src/misc.ts
+++ front/src/misc.ts
@@ -1,2 +1,20 @@
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay))
+
+const debounceMap = new Map<string, ReturnType<typeof setTimeout>>()
+
+export const debounce = (func: () => void, timeout = 300) => {
+  const key = func.toString()
+
+  const timer = debounceMap.get(key)
+  return () => {
+    if (timer !== undefined) {
+      clearTimeout(timer)
+    }
+    const newTimer = setTimeout(() => {
+      func()
+      debounceMap.delete(key)
+    }, timeout)
+    debounceMap.set(key, newTimer)
+  }
+}
