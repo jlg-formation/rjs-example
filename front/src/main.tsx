8c038d00aeb2e923babca0d69da4673e3353ab1e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-22 11:45:37 +0200

    dev: added i18n to homeview

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './font-awesome'
 import App from './App'
+import './font-awesome'
 import './index.scss'
+
+import './i18n/i18n'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- front/src/router/views/HomeView.tsx
+++ front/src/router/views/HomeView.tsx
@@ -2,15 +2,20 @@ import { css } from '@emotion/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 
-const HomeView = () => (
-  <main css={s}>
-    <h1>Gérer efficacement votre stock !</h1>
-    <Link to="/stock" className="button primary">
-      <span>Voir le stock</span>
-      <FontAwesomeIcon icon="angle-right" />
-    </Link>
-  </main>
-)
+import { useTranslation } from 'react-i18next'
+
+const HomeView = () => {
+  const { t } = useTranslation()
+  return (
+    <main css={s}>
+      <h1>{t('Gérer efficacement votre stock !')}</h1>
+      <Link to="/stock" className="button primary">
+        <span>{t('Voir le stock')}</span>
+        <FontAwesomeIcon icon="angle-right" />
+      </Link>
+    </main>
+  )
+}
 
 export default HomeView
 
