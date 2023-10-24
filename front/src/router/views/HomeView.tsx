673f2f0a950c971fc6eb08880a4b3225e69f2b0d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 17:20:24 +0200

    dev: i18n is working

--- front/src/router/views/HomeView.tsx
+++ front/src/router/views/HomeView.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 const HomeView = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation(['home'])
   return (
     <main css={s}>
       <h1>{t('Gérer efficacement votre stock !')}</h1>
