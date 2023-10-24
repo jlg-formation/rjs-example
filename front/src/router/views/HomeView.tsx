822c33d8d4bd520e266b4bd4f68977892df7fc06
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 17:16:56 +0200

    dev: started i18n

--- front/src/router/views/HomeView.tsx
+++ front/src/router/views/HomeView.tsx
@@ -2,16 +2,20 @@ import { css } from '@emotion/react'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-const HomeView = () => (
-  <main css={s}>
-    <h1>Gérer efficacement votre stock !</h1>
-    <Link to="/stock" className="button primary">
-      <span>Voir le stock</span>
-      <FontAwesomeIcon icon={faAngleRight} />
-    </Link>
-  </main>
-)
+const HomeView = () => {
+  const { t } = useTranslation()
+  return (
+    <main css={s}>
+      <h1>{t('Gérer efficacement votre stock !')}</h1>
+      <Link to="/stock" className="button primary">
+        <span>Voir le stock</span>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </Link>
+    </main>
+  )
+}
 
 export default HomeView
 
