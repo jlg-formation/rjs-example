33468481e09edabe878ecbfbeba9d41fcae07de0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 10:24:46 +0200

    dev: added login form

--- front/src/user/views/LoginView.tsx
+++ front/src/user/views/LoginView.tsx
@@ -2,6 +2,17 @@ const LoginView = () => {
   return (
     <main>
       <h1>Login</h1>
+      <form>
+        <label>
+          <span>Login</span>
+          <input type="text" name="login" />
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="password" name="password" />
+        </label>
+        <button className="primary">Se connecter</button>
+      </form>
     </main>
   )
 }
