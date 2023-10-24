b994595e630c64f0413bb36c1f5e9f4011159d07
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 11:22:21 +0200

    dev: use defaultValue

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import { router } from './router/AppRouter'
-import { UserContext } from './user/UserContext'
+import { UserContext, UserContextType } from './user/UserContext'
 import { sleep } from './misc'
 import { useState } from 'react'
 import { User } from './user/interfaces/User'
@@ -9,18 +9,18 @@ import { User } from './user/interfaces/User'
 function App() {
   const [user, setUser] = useState<User | undefined>(undefined)
 
+  const defaultValue: UserContextType = {
+    user: user,
+    login: async () => {
+      console.log('je me loggue')
+      await sleep(2000)
+      console.log('ca y est je suis loggué')
+      setUser({ displayName: 'Eric Durand' })
+    },
+  }
+
   return (
-    <UserContext.Provider
-      value={{
-        user: user,
-        login: async () => {
-          console.log('je me loggue')
-          await sleep(2000)
-          console.log('ca y est je suis loggué')
-          setUser({ displayName: 'Eric Durand' })
-        },
-      }}
-    >
+    <UserContext.Provider value={defaultValue}>
       <RouterProvider router={router} />
     </UserContext.Provider>
   )
