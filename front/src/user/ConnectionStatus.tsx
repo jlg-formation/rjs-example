081f58675da7f2b0dc4ac7cbb88ba57e6a869172
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 10:22:09 +0200

    dev: button in header css

--- front/src/user/ConnectionStatus.tsx
+++ front/src/user/ConnectionStatus.tsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom'
-import s from './ConnectionStatus.module.scss'
 import { User } from './interfaces/User'
 
 const ConnectionStatus = (props: { user: User | undefined }) => {
   return (
     <div>
       {props.user === undefined ? (
-        <Link to="/user/login" className={'button ' + s.button}>
+        <Link to="/user/login" className="button">
           Se connecter
         </Link>
       ) : (
