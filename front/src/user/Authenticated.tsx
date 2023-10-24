17a95f6815ca156b3ed62488729a0212fcf37987
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-24 14:25:11 +0200

    dev: added Navigate component

--- front/src/user/Authenticated.tsx
+++ front/src/user/Authenticated.tsx
@@ -1,24 +1,11 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
+import { Navigate } from 'react-router'
 import { useUser } from './UserContext'
 
 const Authenticated = (props: {
   children: string | JSX.Element | JSX.Element[]
 }) => {
   const { user } = useUser()
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (user === undefined) {
-      navigate('/')
-    }
-  }, [])
-
-  if (user === undefined) {
-    return <></>
-  }
-
-  return <>{props.children}</>
+  return <>{user ? props.children : <Navigate to="/" replace={true} />}</>
 }
 
 export default Authenticated
