71d7ca9421fe7b3026be58d33eb47560b8d1306e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-25 09:59:47 +0200

    dev: added language switch button in utilities

---
+++ front/src/i18n/LanguageSwitch.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import i18n from './i18n'
+
+const LanguageSwitch = () => {
+  const [language, setLanguage] = useState(i18n.language)
+
+  const handleClick = async (): Promise<void> => {
+    const newLanguage = i18n.language === 'en' ? 'fr' : 'en'
+    console.log('newLanguage: ', newLanguage)
+    await i18n.changeLanguage(newLanguage)
+    setLanguage(i18n.language)
+  }
+
+  return <button onClick={handleClick}>{language}</button>
+}
+
+export default LanguageSwitch
--- front/src/layout/UtilityLayout.tsx
+++ front/src/layout/UtilityLayout.tsx
@@ -1,11 +1,13 @@
+import LanguageSwitch from '../i18n/LanguageSwitch'
 import ConnectionStatus from '../user/ConnectionStatus'
 import { useUser } from '../user/UserContext'
 
 const UtilityLayout = () => {
   const { user } = useUser()
   return (
-    <div>
+    <div className="utilities">
       <ConnectionStatus user={user} />
+      <LanguageSwitch />
     </div>
   )
 }
