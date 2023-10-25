b994b884edaebc2b74f2ccaa92277b160d8b555f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-25 17:09:01 +0200

    dev: added test cypress

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000')
+    cy.contains('en').click()
+    cy.contains('Bien gérer efficacement votre stock !')
+    cy.contains('.button', 'Voir le stock').click()
+    cy.get('input[name="login"]').clear().type('jc')
+    cy.contains('button', 'Se connecter').click()
   })
 })
